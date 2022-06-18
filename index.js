1002b34cb38b80f4a83904d1cf667451b6430aa3
mutemip <paulmutemi2030@gmail.com>
2022-06-18 21:50:56 +0300

    practiced function mutation cases

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+  name: "Paul",
+  streetAddress: "Bima Road",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  const newObj = {...employee};
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+  const newObj = Object.assign({}, employee);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee[key];
+  return employee;
+}
